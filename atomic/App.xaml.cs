b3f65a5c43faa2257c4f38e41a39b44876f3b17c
Zuy3646 <Bender200180@gmail.com>
2023-02-28 23:00:22 +0300

    Initial commit

---
+++ atomic/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace atomic
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const WindowState normal = WindowState.Normal;
+
+        public WindowState WindowState { get; private set; }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && this.WindowState == normal)
+            {
+                this.WindowState = WindowState.Maximized;
+
+            }
+            else if (e.ClickCount == 2 && this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+
+            }
+        }
+    }
+}
---
+++ atomic/Atomic/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Atomic.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atomic
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PackIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        void hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://atomicheart.vkplay.ru/"); //открытие ссылки в браузере
+
+        }
+        private void Move_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void AboutUs_Click(object sender, RoutedEventArgs e)
+        {
+            AboutUs aboutUs = new AboutUs();
+            aboutUs.Show();
+            Close();
+        }
+        private void Multimedia_Click(object sender, RoutedEventArgs e)
+        {
+            Multimedia multimedia = new Multimedia();
+            multimedia.Show();
+            Close();
+        }
+    }
+}
+
